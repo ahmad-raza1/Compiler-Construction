575d013236b0137b43c6139a230e9bd482a3c6f7
Ahmad Raza <ahmadraza_@outlook.com>
2020-11-04 22:51:19 +0500

    Read states array

--- Lexical Analyzer/Source.cpp
+++ Lexical Analyzer/Source.cpp
@@ -1,6 +1,17 @@
 #include "SymbolTable.h"
 
 #define	TOTAL_ASCII	128
+#define	STATES	51		// total no of states
+
+enum finalState {
+	LT = -1, LTGT = 2, LTLT = 3, GT = -4, GTGT = 5,
+	ET = -6, ETET = 7, ETP = 8, ETGT = 9, ETLT = 10,
+	NET = 11, ASTERIK = 12, C = -13, CC = 14, CET = 15,
+	FWDSLASH = 16, P = -17, PP = 18, M = -19, MM = 20,
+	AA = 21, VLVL = 22, MOD = 23, ID = -24, SOB = 25,
+	MOB = 26, ROB = 27, SCB = 28, MCB = 29, RCB = 30,
+	SN = -31
+};
 
 void removeSpaces(string&);
 void initilaizeArray(int[], int, int);
@@ -11,6 +22,7 @@ int** read2DArrayFromFile(string, int&, int&);
 void print2DArray(int**, int, int);
 void readKeywordsinSymbolTable(string, SymbolTable&);
 void readAsciiToIntIndexMappingArray(string, int[]);
+void readStatesArray(string, int[]);
 
 // Driver code
 int main()
@@ -18,22 +30,31 @@ int main()
 	SymbolTable table;
 
 	readKeywordsinSymbolTable("keywords.csv", table);
+	cout << "Keywords in hash table...\n" << endl;
 	table.print();
 
 	int mapArr[TOTAL_ASCII];
-	initilaizeArray(mapArr, TOTAL_ASCII, -1);
 
+	initilaizeArray(mapArr, TOTAL_ASCII, -1);
 	readAsciiToIntIndexMappingArray("mapping.txt", mapArr);
+	cout << "Mapping Array...\n" << endl;
 	printArr(mapArr, TOTAL_ASCII);
+	
 	int ** TransitionTable, row = 0, col = 0;
 	
 	TransitionTable = read2DArrayFromFile("transition-table.txt", row, col);
+	cout << "Transition table...\n" << endl;
 	print2DArray(TransitionTable, row, col);
-
 	deallocateDblArr(TransitionTable, row, col);
 
+	int final_nonfinal[STATES];
+	
+	initilaizeArray(final_nonfinal, STATES, 0);
+	readStatesArray("states.txt", final_nonfinal);
+	cout << "States Array...\n" << endl;
+	printArr(final_nonfinal, STATES);
 
-
+	
 	return 0;
 }
 
@@ -155,6 +176,19 @@ void readAsciiToIntIndexMappingArray(string filename, int mapArr[])
 		else
 			mapArr[int(c)] = index;
 	}
+
+	fin.close();
+}
+
+void readStatesArray(string filename, int states[])
+{
+	ifstream fin(filename);
+
+	int size = 0, state;
+	fin >> size;
+
+	for (int i = 0; i < size; i++)
+		fin >> state >> states[i];
 
 	fin.close();
 }
